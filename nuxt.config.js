7bb24eea747065f2349d3759015abb6e37f83dc3
tchret <tchret@gmail.com>
2019-06-13 23:28:59 +0300

    fix particles

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,7 +20,7 @@ export default {
     ],
     script: [
       { src: 'https://apis.google.com/js/platform.js' },
-      { src: 'http://www.jsdelivr.com/#!particles.js' }
+      { src: 'https://cdn.jsdelivr.net/npm/particles.js@2.0.0/particles.js' }
     ]
   },
   /*
